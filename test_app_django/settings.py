ef1546d524f0a2f6d1d8c2695eadfa567fb95b4b
abukawsar45 <kawsar1146236@gmail.com>
2024-02-06 23:16:21 +0600

    add allowed host link

--- test_app_django/settings.py
+++ test_app_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
-
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv() 
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://my-services-e6eo.onrender.com]
 
 
 # Application definition
@@ -83,10 +83,11 @@ WSGI_APPLICATION = 'test_app_django.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': dj_database_url.config(
+        default='postgres://kawsar:QDtqEIckfPt9cUW6AZqh7vfYi4HAOI8O@dpg-cn15k5da73kc73eg4040-a.oregon-postgres.render.com/maindatabase_l473',
+        conn_max_age=600,
+        conn_health_checks=True,
+   )
 }
 
 
